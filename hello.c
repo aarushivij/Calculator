55cfd35fac38e65d8f400077e7794169236be7e1
aarushivij <42747230+aarushivij@users.noreply.github.com>
2018-09-18 23:15:18 +0530

    Updated hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,23 @@
-int main(char* args){
- 	print("Hi");
- 	return 0;
+//Calculator
+#include<iostream>
+using namespace std;
+int main()
+{
+double a,b,A;
+char c;
+cout<<"Enter two numbers:"<<endl;
+cout<<"Press (+) for Addition \n (-) for Subtraction \n (*) for Multiplication \n (/) for Division "<<endl;
+cin>>c;
+cin>>a>>c>>b;
+if(c=='+')
+A=a+b;
+else if (c=='-')
+A=a-b;
+else if(c=='*')
+A=a*b;
+else if(c=='/')
+A=a/b;
+cout<<"The answer is:"<<A;
+system ("pause");
+return 0;
 }
